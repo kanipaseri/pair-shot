7d4e473da3bd946bd92ff3996a4e8d53e2bbc6ef
kanipaseri <kanipaseri13@gmail.com>
2026-09-23 02:15:37 +0900

    Bump PWA cache for ratio switch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pair-shot-v2';
+const CACHE = 'pair-shot-v3';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
